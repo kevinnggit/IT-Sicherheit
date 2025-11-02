a64f413c9c31ce96ba56b3a3322ae86f7faefa33
Kevinnguefack <nguefackkevin66@gmail.com>
2025-11-02 14:59:00 +0100

    debug

--- install.sh
+++ install.sh
@@ -3,33 +3,50 @@
 TEAM_NR="12"
 HOST_IP=$1
 
-CURRENT_DIR_NAME="IT-Sicherheit"
+PROJECT_DIR_NAME="IT-Sicherheit"
 TARGET_DIR_NAME="team${TEAM_NR}"
 ARCHIVE_NAME="${TARGET_DIR_NAME}-etp.tar.gz"
 
-
-if [ "$TEAM_NR" != "12" ]; then
-    echo "[Fehler] Bitte passe die TEAM_NR Variable in install.sh an."
+# Host-IP als Argument übergeben ?
+if [ -z "$HOST_IP" ]; then
+    echo "[Fehler] Du musst die IP-Adresse des Zielhosts als Argument angeben."
+    echo "Beispiel: ./install.sh 10.42.1.101"
     exit 1
 fi
 
-cd ..
+echo "[Info] Benenne '${PROJECT_DIR_NAME}' temporär um zu '${TARGET_DIR_NAME}'..."
+mv "$PROJECT_DIR_NAME" "$TARGET_DIR_NAME"
 
-mv "$CURRENT_DIR_NAME" "$TARGET_DIR_NAME"
+# Erstelle das Archiv mit dem umbenannten Ordner
+tar -czf "$ARCHIVE_NAME" \
+    --exclude="${TARGET_DIR_NAME}/bin" \
+    --exclude="${TARGET_DIR_NAME}/IT_S" \
+    "$TARGET_DIR_NAME"
 
-tar -czf "$ARCHIVE_NAME" --exclude='./bin' --exclude='./IT_S' "$TARGET_DIR_NAME"
+echo "[Info] Benenne '${TARGET_DIR_NAME}' zurück zu '${PROJECT_DIR_NAME}'..."
+mv "$TARGET_DIR_NAME" "$PROJECT_DIR_NAME"
 
-mv "$TARGET_DIR_NAME" "$CURRENT_DIR_NAME"
-
-scp "$ARCHIVE_NAME" "root@$HOST_IP:"      # ~/
+scp "$ARCHIVE_NAME" "root@$HOST_IP:"
 
 if [ $? -ne 0 ]; then
     echo "[Fehler] Dateiübertragung fehlgeschlagen."
-    rm "$ARCHIVE_NAME"
-    cd "$CURRENT_DIR_NAME"
+    rm "$ARCHIVE_NAME" # Räume lokales Archiv auf
     exit 1
 fi
 
 echo "[Info] Dateiübertragung erfolgreich."
-#rm "$ARCHIVE_NAME"
-cd "$CURRENT_DIR_NAME"
+# rm "$ARCHIVE_NAME"
+
+
+echo "[Info] Entpacke Archiv auf dem Zielhost unter /test/..."
+
+ssh root@$HOST_IP "mkdir -p /root/test && tar -xvzf /root/${ARCHIVE_NAME} -C /root/test"
+
+if [ $? -eq 0 ]; then
+    echo "[Info] Archiv wurde erfolgreich auf dem Zielhost entpackt."
+else
+    echo "[Fehler] Entpacken auf dem Zielhost fehlgeschlagen."
+fi
+
+
+echo "--- Fertig ---"
